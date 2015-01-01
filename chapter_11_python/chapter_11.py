57a774a24f831bb165c9aa0d199944a652acc1ea
David Weinberger <davidtweinberger@gmail.com>
2014-12-31 19:21:33 -0500

    chapter 11

---
+++ chapter_11_python/chapter_11.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+"""
+Answers to the questions in chapter 11 of Cracking the Coding Interview, in Python
+
+David Weinberger (12/31/2014)
+Brown University '16
+davidtweinberger@gmail.com 
+"""
+
+def mergeAndSort(A, B):
+	pass
+
+
+def main():
+	#tests go here
+
+	#Problem 11.1
+	print mergeAndSort([4, 8, 1, 9, 3, None, None, None, None, None], [2, 6, 1, 3, 9])
+
+if __name__ == '__main__':
+	main()
