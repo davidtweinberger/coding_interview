01a3e54b841cf4c2ede3b5c496dfdafe00a0e67e
David Weinberger <davidtweinberger@gmail.com>
2015-01-05 15:36:10 -0500

    chapter 1 java

--- chapter_1_java/StringManipulator.java
+++ chapter_1_java/StringManipulator.java
@@ -2,29 +2,132 @@ package chapter_1_java;
 
 public class StringManipulator {
 
-	private String _string;
-
-	public StringManipulator(String s){
-		_string = s;
-	}
-
-	public void setString(String s){
-		_string = s;
-	}
-
 	//Methods for chapter 1
-	public Boolean uniqueChars(){
+	public Boolean uniqueChars(String string){
 		//Set will contain letters - use the Character type for the generic (cannot use primitive char)
 		java.util.HashSet<Character> set = new java.util.HashSet<Character>();
 		//Iterates through the string
-		for (int i=0; i<_string.length(); i++){
-			if (set.add(_string.charAt(i)) == false){
+		for (int i=0; i<string.length(); i++){
+			if (set.add(string.charAt(i)) == false){
 				return false;
 			}
 		}
 		//finished, all chars are unique
 		return true;
 
+	}
+
+	public String reverse(String string){
+		//Turns string into a char array
+		char[] str = string.toCharArray();
+		int len = str.length;
+
+		//Iterates and switches
+		for (int i=0; i<len/2; i++){
+			char temp = str[len - i - 1];
+			str[len - i - 1] = str[i];
+			str[i] = temp;
+		}
+
+		//makes string
+		string = new String(str);
+		return string;
+	}
+
+	public Boolean angrams(String s1, String s2){
+		//table mapping from char to number of occurrences
+		java.util.Hashtable<Character, Integer> table = new java.util.Hashtable<Character, Integer>();
+
+		//add s1 to the table
+		char[] str1 = s1.toCharArray();
+		int len1 = str1.length;
+		for (int i=0; i<len1; i++){
+			if (table.get(str1[i]) != null){
+				table.put(str1[i], table.get(str1[i]) + 1);
+			}
+			else {
+				table.put(str1[i], 1);
+			}
+		}
+
+		//remove s2 from the table, checking for differences
+		char[] str2 = s2.toCharArray();
+		int len2 = str2.length;
+		for (int j=0; j < len2; j++){
+			if (table.containsKey(str2[j]) == false){
+				return false;
+			}
+			table.put(str2[j], table.get(str2[j]) - 1);
+			if (table.get(str2[j]) == 0){
+				table.remove(str2[j]);
+			}
+		}
+
+		//if table is now empty, strings were equal
+		if (table.isEmpty()){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	/*
+		Replaces all spaces in string with "%20"
+		Args: string, the length of the "true string"
+	*/
+	public String replaceSpaces(String string, int stringlength){
+		char[] str = string.toCharArray();
+		//error checking
+		if (str.length == 0){
+			return "";
+		}
+
+		//represents the end of the string
+		int end = str.length - 1;
+		
+		//last character that is not a space
+		int curr = stringlength-1;
+
+		//iterates backwards, filling in the string with %20
+		while (curr > 0){
+			if (!(str[curr] == ' ')){
+				str[end] = str[curr];
+				end--;
+				curr--;
+			} else {
+				curr--;
+				str[end] = '0';
+				str[end-1] = '2';
+				str[end-2] = '%';
+				end -= 3;
+			}
+		}
+
+		return new String(str);
+
+	}
+
+	public String compress(String s){
+		//used to concatenate the compressed string
+		java.lang.StringBuilder sb = new java.lang.StringBuilder();
+		char prev = s.charAt(0);
+		char curr = s.charAt(0);
+		int count = 0;
+		for (int i=0; i<s.length(); i++){
+			curr = s.charAt(i);
+			if (curr != prev){
+				sb.append(prev);
+				sb.append(count);
+				prev = curr;
+				count = 1;
+			} else {
+				count += 1;
+			}
+		}
+		sb.append(prev);
+		sb.append(count);
+		return sb.toString().length() < s.length() ? sb.toString() : s;
 	}
 
 }
--- chapter_1_java/chapter_1.java
+++ chapter_1_java/chapter_1.java
@@ -6,20 +6,39 @@ public class chapter_1 {
 
 	public chapter_1(){
 		System.out.println("Running tests for chapter 1.");
+		_sm = new StringManipulator();
 
 		//Problem 1.1
-		_sm = new StringManipulator("David");
-		assert (_sm.uniqueChars() == true) : "Error in Problem 1";
-		_sm.setString("DAVID");
-		assert (_sm.uniqueChars() == false) : "Error in Problem 1";
-		_sm.setString("   DD       avid .../////");
-		assert (_sm.uniqueChars() == false) : "Error in Problem 1";
-		_sm.setString("1234567890qwertyuiop-=[]asdfghjkl;'zxcvbnm,./?><:|}{+_");
-		assert (_sm.uniqueChars() == true) : "Error in Problem 1";
+		assert (_sm.uniqueChars("David") == true) : "Error in Problem 1";
+		assert (_sm.uniqueChars("DAVID") == false) : "Error in Problem 1";
+		assert (_sm.uniqueChars("   DD       avid .../////") == false) : "Error in Problem 1";
+		assert (_sm.uniqueChars("1234567890qwertyuiop-=[]asdfghjkl;'zxcvbnm,./?><:|}{+_ ") == true) : "Error in Problem 1";
 
 		//Problem 1.2
+		assert (_sm.reverse("DAVID").equals("DIVAD")) : "Error in Problem 2";
+		assert (_sm.reverse("A man, a plan, a canal, panama").equals("amanap ,lanac a ,nalp a ,nam A")) : "Error in Problem 2";
+		assert (_sm.reverse("This is a simple test.").equals(".tset elpmis a si sihT")) : "Error in Problem 2";
 
+		//Problem 1.3
+		assert (_sm.angrams("David", "aidDv") == true) : "Error in Problem 3";
+		assert (_sm.angrams("David", "aiDv") == false) : "Error in Problem 3";
+		assert (_sm.angrams("David is writing this code", "    acDddeghiiiiinorssttvw") == true) : "Error in Problem 3";
+		assert (_sm.angrams("", "") == true) : "Error in Problem 3";
+
+		//Problem 1.4
+		assert (_sm.replaceSpaces("This is a small test.        ", 21).equals("This%20is%20a%20small%20test.")) : "Error in Problem 4";
+		assert (_sm.replaceSpaces("Mr John Smith    ", 13).equals("Mr%20John%20Smith")) : "Error in Problem 4";
+
+		//Problem 1.5
+		assert (_sm.compress("aaabbbbcccccdef").equals("a3b4c5d1e1f1")) : "Error in Problem 5";
+		assert (_sm.compress("asdfaaabbbcccdddeeefffggghhhiiijjjzxcv").equals("a1s1d1f1a3b3c3d3e3f3g3h3i3j3z1x1c1v1")) : "Error in Problem 5";
+		assert (_sm.compress("asdfghjkl").equals("asdfghjkl")) : "Error in Problem 5";
 		System.out.println("All tests passed.");
+
+		//TODO
+		//Problem 1.6
+		//Problem 1.7
+		//Problem 1.8
 	}
 
 	public static void main(String[] args) {
