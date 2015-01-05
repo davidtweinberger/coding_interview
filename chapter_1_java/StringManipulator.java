1d2cf38595789f5c46085d86b6ceb4008a3c46dc
David Weinberger <davidtweinberger@gmail.com>
2015-01-05 12:41:55 -0500

    first java commit

---
+++ chapter_1_java/StringManipulator.java
@@ -0,0 +1,17 @@
+package chapter_1_java;
+
+public class StringManipulator {
+
+	private String _string;
+
+	public void StringManipulator(String s){
+		_string = s;
+	}
+
+	public void setString(String s){
+		_string = s;
+	}
+
+	//Methods for chapter 1
+
+}
---
+++ chapter_1_java/chapter_1.java
@@ -0,0 +1,16 @@
+package chapter_1_java;
+
+public class chapter_1 {
+
+	public chapter_1(){
+		System.out.println("Running tests for chapter 1");
+		//Test cases for chapter 1
+
+		System.out.println("All tests passed.");
+	}
+
+	public static void main(String[] args) {
+		new chapter_1();
+	}
+
+}
