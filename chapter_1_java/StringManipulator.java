48c65d03f58064e838808b01c9345d4462b675c6
David Weinberger <davidtweinberger@gmail.com>
2015-01-05 13:27:00 -0500

    chapter 1 java

--- chapter_1_java/StringManipulator.java
+++ chapter_1_java/StringManipulator.java
@@ -4,7 +4,7 @@ public class StringManipulator {
 
 	private String _string;
 
-	public void StringManipulator(String s){
+	public StringManipulator(String s){
 		_string = s;
 	}
 
@@ -13,5 +13,18 @@ public class StringManipulator {
 	}
 
 	//Methods for chapter 1
+	public Boolean uniqueChars(){
+		//Set will contain letters - use the Character type for the generic (cannot use primitive char)
+		java.util.HashSet<Character> set = new java.util.HashSet<Character>();
+		//Iterates through the string
+		for (int i=0; i<_string.length(); i++){
+			if (set.add(_string.charAt(i)) == false){
+				return false;
+			}
+		}
+		//finished, all chars are unique
+		return true;
+
+	}
 
 }
--- chapter_1_java/chapter_1.java
+++ chapter_1_java/chapter_1.java
@@ -2,9 +2,22 @@ package chapter_1_java;
 
 public class chapter_1 {
 
+	private StringManipulator _sm;
+
 	public chapter_1(){
-		System.out.println("Running tests for chapter 1");
-		//Test cases for chapter 1
+		System.out.println("Running tests for chapter 1.");
+
+		//Problem 1.1
+		_sm = new StringManipulator("David");
+		assert (_sm.uniqueChars() == true) : "Error in Problem 1";
+		_sm.setString("DAVID");
+		assert (_sm.uniqueChars() == false) : "Error in Problem 1";
+		_sm.setString("   DD       avid .../////");
+		assert (_sm.uniqueChars() == false) : "Error in Problem 1";
+		_sm.setString("1234567890qwertyuiop-=[]asdfghjkl;'zxcvbnm,./?><:|}{+_");
+		assert (_sm.uniqueChars() == true) : "Error in Problem 1";
+
+		//Problem 1.2
 
 		System.out.println("All tests passed.");
 	}
