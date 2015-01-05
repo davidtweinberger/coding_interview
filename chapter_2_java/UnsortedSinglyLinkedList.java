e4962839f5ad2a6ca997da56ca74f08b097ecce0
David Weinberger <davidtweinberger@gmail.com>
2015-01-05 16:40:28 -0500

    singly linked list - TODO testing

---
+++ chapter_2_java/UnsortedSinglyLinkedList.java
@@ -0,0 +1,125 @@
+package chapter_2_java;
+
+public class UnsortedSinglyLinkedList<T extends java.lang.Object> {
+
+	private Node<T> _head;
+
+	//basic constructor for the linked list
+	public UnsortedSinglyLinkedList(){
+		_head = null;
+	}
+
+	//other constructors
+	public UnsortedSinglyLinkedList(T[] elements){
+		_head = null;
+		for (int i=0; i<elements.length; i++){
+			insertAtTail(elements[i]);
+		}
+	}
+
+	//inner class representing a node in the list
+	private class Node<T>{
+
+		//node ivars
+		private T _data;
+		private Node<T> _next;
+
+		//4 constructors
+		public Node(){
+			_data = null;
+			_next = null;
+		}
+		public Node(T data){
+			_data = data;
+			_next = null;
+		}
+		public Node(Node<T> next){
+			_data = null;
+			_next = next;
+		}
+		public Node(T data, Node<T> next){
+			_data = data;
+			_next = next;
+		}
+
+		//accessors and mutators
+		public T getData(){
+			return _data;
+		}
+
+		public void setData(T data){
+			_data = data;
+		}
+
+		public Node<T> getNext(){
+			return _next;
+		}
+
+		public void setNext(Node<T> next){
+			_next = next;
+		}
+	}
+
+	public Boolean isEmpty(){
+		return (_head == null);
+	}
+
+	public Boolean insertAtTail(T data){
+		if (_head == null){
+			_head = new Node<T>(data);
+		} else {
+			Node<T> curr = _head;
+			while (curr.getNext() != null){
+				curr = curr.getNext();
+			}
+			curr.setNext(new Node<T>(data));
+		}
+		return true;
+	}
+
+	public Boolean insertAtHead(T data){
+		_head = new Node<T>(data, _head);
+		return true;
+	}
+
+	public Boolean remove(T data){
+		if (data == null){
+			return false;
+		} else if (_head == null){
+			return false;
+		} else if (_head.getData().equals(data)){
+			_head = _head.getNext();
+			return true;
+		} else {
+			Node<T> curr = _head;
+			while ((curr.getNext() != null) && (curr.getNext().getData().equals(data) == false)){
+				curr = curr.getNext();
+			}
+			if (curr.getNext() != null){
+				return false;
+			}
+			else {
+				curr.setNext(curr.getNext().getNext());
+				return true;
+			}
+		}
+	}
+
+	public Boolean contains(T data){
+		if (data == null){
+			return false;
+		} else if (_head == null){
+			return false;
+		} else {
+			Node<T> curr = _head;
+			while (curr.getData().equals(data) == false){
+				curr = curr.getNext();
+				if (curr == null){
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+}
---
+++ chapter_2_java/chapter_2.java
@@ -0,0 +1,15 @@
+package chapter_2_java;
+
+public class chapter_2 {
+
+	public chapter_2(){
+		System.out.println("Running tests for chapter 2.");
+		//test cases
+
+		System.out.println("All tests passed.");
+	}
+
+	public static void main(String[] args) {
+		new chapter_2();
+	}
+}
